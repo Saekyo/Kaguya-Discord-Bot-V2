ea2e05e412b2404299b3fa522e39a4ef68b0f682
Suryo Mujahid <suryomujahid@gmail.com>
2022-10-19 17:41:53 +0700

    feat(localization): administration in bahasa

---
+++ Languages/ID/Administration/language.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    choose      : "Berikut merupakan daftar bahasa yang tersedia",
+    howUse      : "Tulis bahasa code yang ingin anda gunakan",
+    timeEnd     : "Waktu habis, panggil perintah lagi",
+    done        : "Bahasa telah diubah",
+    new         : "Sekarang saya akan berbicara dalam bahasa"
+
+};
---
+++ Languages/ID/Administration/prefix.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    alreadyUse  : "Kamu sedang menggunakan prefix ini",
+    changed     : "Prefix telah diubah",
+    newPrefix   : ( prefix ) => `Sekarang gunakan \`${prefix}\` untuk menjalankan perintah`,
+    notModified : "Prefix tidak diubah",
+    error       : "Sepertinya terjadi kesalahan ..., panggil komandan!"
+
+};
